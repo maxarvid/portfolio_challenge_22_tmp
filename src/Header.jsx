ca5408a419eb9038587c2c801e62182b0d65b50b
Roberto <maisto_roberto@hotmail.it>
2022-02-09 15:09:09 +0100

    adding new files header footer hello jsx

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+// src/Header.jsx
+
+import React from "react";
+
+const Header = () => {
+    return (
+        <nav>
+            <h1 id="Header">My Portfolio</h1>
+        </nav>
+    );
+};
+
+export default Header;
