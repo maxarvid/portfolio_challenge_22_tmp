a5dea60bbdfe62be35ad2f771cc2f850c88cd9bd
Roberto <maisto_roberto@hotmail.it>
2022-02-11 10:12:58 +0100

    Another step add

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
+import axios from "axios";
 
-const Projects = () => {
+class Projects extends Component {
+  state = {
+    projects: [
+      {
+        id: 1,
+        name: "My First Website",
+      },
+      {
+        id: 2,
+        name: "Fizzbuzz",
+      },
+    ],
+  };
+
+  render() {
+    const { projects } = this.state;
+
+    let projectsList = projects.map((project) => {
+      return (
+        <div id={`project-${project.id}`} key={project.id}>
+                              <h3>{project.name}</h3>
+                          
+        </div>
+      );
+    });
+
     return (
-        <Container>
-            <h1 id="projects-header">My Projects</h1>
-            <p>
-                Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident copporti fuga saepe
-                distinctio ipsum? Et quos haram excepturi dolorum molestias?
-            </p>
-        </Container>
+      <Container>
+                        <h1 id="projects-header">My Projects</h1>
+                        {projectsList}
+                    
+      </Container>
     );
-};
+  }
+}
 
 export default Projects;
