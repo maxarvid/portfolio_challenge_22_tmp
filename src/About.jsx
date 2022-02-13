9bb071d4d5562678e4b5b7f7ced06951888463b2
Roberto <maisto_roberto@hotmail.it>
2022-02-13 17:21:48 +0100

    Adding some information about me

--- src/About.jsx
+++ src/About.jsx
@@ -6,9 +6,13 @@ const About = () => {
     <Container>
       <h1 id="about-header">About Me</h1>
       <p>
-        Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident
-        copporti fuga saepe distinctio ipsum? Et quos haram excepturi dolorum
-        molestias?
+        Hi there!
+        Here I am.
+        My name is Roberto and I decided to take this course because I have always been passionate about technology.
+        I've always wondered, just as I watch a film, another passion of mine, what's behind it?
+        How is what I'm watching created?
+        How can I make a contribution of my own to create something that we usually see on the screen and take for granted?
+        I hope I can go deeper and deeper and always create something new!
       </p>
     </Container>
   );
