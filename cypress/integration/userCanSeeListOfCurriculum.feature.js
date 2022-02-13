3c0d522494b2ba3f342c16aeaf114dc89dbb9063
Roberto <maisto_roberto@hotmail.it>
2022-02-13 18:30:20 +0100

    Adding CvCard.jsx

---
+++ cypress/integration/userCanSeeListOfCurriculum.feature.js
@@ -0,0 +1,23 @@
+describe("User can see experience", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000");
+        cy.get("#cv-tab").click();
+    });
+
+    it("displays basic information", () => {
+        cy.get("#basics").within(() => {
+            cy.get(".name").should("contain", "Roberto Maisto");
+            cy.get(".label").should("contain", "Junior Developer");
+            cy.get(".email").should("contain", "maisto_roberto@hotmail.it");
+        });
+     });
+
+     it("displays previous work experience", () => {
+        cy.get("#work").within(() => {
+            cy.get(".name").should("contain", "Synlab CAM Polidiagnostico ");
+            cy.get(".position").should("contain", "Graphic designer");
+            cy.get(".summary").should("contain", "Use the Adobe package...");
+        });
+     });
+
+});
--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Hello />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/cv" element={<Cv />} />
       </Routes>
       <Footer />
     </>
